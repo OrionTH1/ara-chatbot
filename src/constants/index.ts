530fec99fb31de69d6d03c84ff9bda663daff596
Matheus Emanoel <buss.matheusemanoel@gmail.com>
2025-05-02 23:53:09 -0300

    clean: remove sidebar constant

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -2,40 +2,11 @@ import {
   BadgeCheck,
   BadgePercent,
   CreditCard,
-  Folder,
   LogOut,
-  MessageSquarePlus,
   Settings,
 } from "lucide-react";
 
 export const sidebarData = {
-  navMain: [
-    {
-      title: "New chat",
-      url: "/chat/new",
-      icon: MessageSquarePlus,
-    },
-    {
-      title: "Chats",
-      url: "#",
-      icon: Folder,
-      isActive: true,
-      items: [
-        {
-          title: "História da fúria",
-          url: "/chat/2131d21as",
-        },
-        {
-          title: "Todos jogos",
-          url: "/chat/2131d21as",
-        },
-        {
-          title: "Curiosidades",
-          url: "/chat/2131d21as",
-        },
-      ],
-    },
-  ],
   userMain: [
     {
       title: "Change your Plan",
