b32d78c2eec97177c019284080ba300d17ff250d
Matheus Emanoel <buss.matheusemanoel@gmail.com>
2025-05-02 23:46:16 -0300

    feat: add markdown render in message

--- src/features/chat/components/Message.tsx
+++ src/features/chat/components/Message.tsx
@@ -1,18 +1,25 @@
+import MarkdownRenderer from "@/components/MarkdownRenderer";
 import { cn } from "@/lib/utils";
+import { useEffect, useRef } from "react";
 
 interface MessageProps {
   text: string;
-  author: "user" | "model";
+  author: Role;
 }
 function Message({ text, author }: MessageProps) {
+  const bottomRef = useRef<HTMLDivElement>(null);
+  useEffect(() => {
+    bottomRef.current?.scrollIntoView({ behavior: "smooth" });
+  }, [text]);
   return (
     <li
       className={cn(
-        "p-4 bg-secondary-background rounded-lg max-w-1/2",
+        "p-4 bg-secondary-background rounded-lg max-w-[85%] lg:max-w-1/2",
         author === "user" ? "self-end" : "self-start"
       )}
     >
-      <p>{text}</p>
+      <MarkdownRenderer content={text} />
+      <div ref={bottomRef} />
     </li>
   );
 }
