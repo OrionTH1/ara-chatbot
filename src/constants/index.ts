f766d92ce5469eb87fbcedefe4e859aa17aec9b3
Matheus Emanoel <buss.matheusemanoel@gmail.com>
2025-05-04 16:34:43 -0300

    clean: remove sidebar user contants

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,28 +1,7 @@
-import {
-  BadgeCheck,
-  BadgePercent,
-  CreditCard,
-  LogOut,
-  Settings,
-} from "lucide-react";
+import { LogOut, Settings } from "lucide-react";
 
 export const sidebarData = {
   userMain: [
-    {
-      title: "Change your Plan",
-      url: "/order?name=",
-      icon: BadgePercent,
-    },
-    {
-      title: "Account",
-      url: "/cloud/settings#user-settings",
-      icon: BadgeCheck,
-    },
-    {
-      title: "Billing",
-      url: "/cloud/settings#user-billing",
-      icon: CreditCard,
-    },
     {
       title: "Settings",
       url: "/cloud/settings",
