cb58a15c82bd6bbc7f0fd24b45c113973e89b8eb
Matheus Emanoel <buss.matheusemanoel@gmail.com>
2025-05-04 16:34:56 -0300

    feat: add max profile size

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -17,3 +17,5 @@ export const sidebarData = {
 
 export const avatarPlacerHolderUrl =
   "https://storage.needpix.com/rsynced_images/avatar-1577909_1280.png";
+
+export const MAX_PROFILE_SIZE = 2 * 1024 * 1024; // 2MB
