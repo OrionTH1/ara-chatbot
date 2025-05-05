3a0f5a2166dada53b84ea58d04513333c445d3a1
Matheus Emanoel <buss.matheusemanoel@gmail.com>
2025-05-04 23:04:21 -0300

    feat: translate the landing page to english

--- src/app/(public)/page.tsx
+++ src/app/(public)/page.tsx
@@ -7,10 +7,8 @@ function LandingPage() {
       <section id="hero" className="flex">
         <div className="flex flex-col items-center lg:items-start flex-1 px-4 lg:pl-20 pt-28 pb-8 ">
           <div className="flex flex-col items-center text-center lg:text-start lg:items-start gap-4 mb-6">
-            <h1 className="heading-1">Conheça a Luna</h1>
-            <p className="text-1">
-              A inteligência artificial, torcedora da FURIA
-            </p>
+            <h1 className="heading-1">Meet the Luna</h1>
+            <p className="text-1">The Artificial Intelligence and Furia Fan</p>
           </div>
 
           <div className="flex gap-4">
@@ -18,13 +16,13 @@ function LandingPage() {
               href="/chat"
               className="flex justify-center items-center heading-3 bg-brand text-primary-foreground hover:bg-brand/90 h-11 px-5 py-4 rounded-md transition-all"
             >
-              Testar agora
+              Test now
             </Link>
             <Link
               href={"#about"}
               className="flex justify-center items-center heading-3 border-2 border-brand bg-transparent hover:bg-brand/90 hover:border-brand/90 h-11 px-5 py-4 rounded-md transition-all "
             >
-              Sobre
+              About
             </Link>
           </div>
         </div>
@@ -44,12 +42,13 @@ function LandingPage() {
         id="about"
         className="flex flex-1 items-center lg:items-start flex-col px-8 lg:pl-20 pt-12 pb-8 bg-secondary-background "
       >
-        <h2 className="heading-1 mb-4">Sobre a Luna</h2>
+        <h2 className="heading-1 mb-4">About Luna</h2>
         <p className="text-1 w-fit lg:w-152 text-center lg:text-start">
-          Salve, Torcedor! A Luna, é uma inteligência artificial, torcedora da
-          FURIA, pronta para tirar todas as suas dúvidas sobre história,
-          modalidades, estatísticas e curiosidades desta organização Brasileira
-          de E-Sports que já trouxe tantas felicidades para o Brasileiro
+          Hey, Fan! Luna is an artificial intelligence and a proud FURIA
+          supporter, ready to answer all your questions about the team&apos;s
+          history, game modes, stats, and fun facts. Get to know this Brazilian
+          eSports organization that has brought so much joy to fans across the
+          country!
         </p>
       </section>
     </main>
